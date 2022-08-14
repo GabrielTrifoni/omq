47a17ee825e4a70082f3d715c6310bfff2644e63
GabrielTrifoni <111247285+GabrielTrifoni@users.noreply.github.com>
2022-08-14 16:26:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+class Musica {
+    constructor(autirtulo, video, duracao) {
+        this.autirtulo = autirtulo;
+        this.video = video;
+        this.duracao = duracao;
+    }
+}
+
+var bluezenith = new Musica("xi - Blue Zenith", "https://www.youtube.com/embed/y2A3hLMWk6M?rel=0&autoplay=1", "256");
+var densetsunomatsuri = new Musica("RUMI - Densetsu no Matsuri", "https://www.youtube.com/embed/gBotPWBG5gw?rel=0&autoplay=1", "169"); 
