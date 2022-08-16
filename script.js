f161bd5adb2c97ba6986bca751ad4a2c8cc4f774
GabrielTrifoni <gabrielsoavetrifoni12@gmail.com>
2022-08-16 01:44:03 -0300

    updated

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ class Song {
 var songList = [];
 var generatedNumbers = [];
 
-songList.push(new Song("xi - Blue Zenith", "https://www.youtube.com/embed/y2A3hLMWk6M?rel=0&autoplay=1", "256"));
-songList.push(new Song("RUMI - Densetsu no Matsuri", "https://www.youtube.com/embed/gBotPWBG5gw?rel=0&autoplay=1", "169"));
-songList.push(new Song("Second Moon - Blue Breeze Blow (Pocari Sweat)", "https://www.youtube.com/embed/nlGVBZiuqL8?rel=0&autoplay=1", "35"));
-songList.push(new Song("Loki - The Victory Is Ours", "https://www.youtube.com/embed/bKUAYI4Ofkg?rel=0&autoplay=1", "175"));
-songList.push(new Song("Red Velvet - Day 1", "https://www.youtube.com/embed/KTaa3Pbv9c0?rel=0&autoplay=1", "206"));
-songList.push(new Song("mili - Vitamins (feat. world's end girlfriend)", "https://www.youtube.com/embed/JCzIM9KUiik?rel=0&autoplay=1", "300"));
-songList.push(new Song("II-L - SPUTNIK-3", "https://www.youtube.com/embed/6x-t_2aBIb8?rel=0&autoplay=1", "173"));
-songList.push(new Song("IOSYS - Miracle Hinacle", "https://www.youtube.com/embed/e_OvCNDWd6A?rel=0&autoplay=1", "199"));
-songList.push(new Song("Pastel * Palletes - Baby Sweet Berry Love", "https://www.youtube.com/embed/6WeBp5G2kUU?rel=0&autoplay=1", "244"));
-songList.push(new Song("Yorushika - Hachigatsu, Bou, Tsukiakari", "https://www.youtube.com/embed/J6eih31MaQ8?rel=0&autoplay=1", "278"));
-songList.push(new Song("Nanahira - Kizuitara Shunkashuutou", "https://www.youtube.com/embed/XRAOSO5m-cY?rel=0&autoplay=1", "295"));
-songList.push(new Song("linear ring - enchanted love", "https://www.youtube.com/embed/qE6mruRB1PA?rel=0&autoplay=1", "130"));
+songList.push(new Song("xi - Blue Zenith", "https://www.youtube.com/embed/y2A3hLMWk6M", "256"));
+songList.push(new Song("RUMI - Densetsu no Matsuri", "https://www.youtube.com/embed/gBotPWBG5gw", "169"));
+songList.push(new Song("Second Moon - Blue Breeze Blow (Pocari Sweat)", "https://www.youtube.com/embed/nlGVBZiuqL8", "35"));
+songList.push(new Song("Loki - The Victory Is Ours", "https://www.youtube.com/embed/bKUAYI4Ofkg", "175"));
+songList.push(new Song("Red Velvet - Day 1", "https://www.youtube.com/embed/KTaa3Pbv9c0", "206"));
+songList.push(new Song("mili - Vitamins (feat. world's end girlfriend)", "https://www.youtube.com/embed/JCzIM9KUiik", "300"));
+songList.push(new Song("II-L - SPUTNIK-3", "https://www.youtube.com/embed/6x-t_2aBIb8", "173"));
+songList.push(new Song("IOSYS - Miracle Hinacle", "https://www.youtube.com/embed/e_OvCNDWd6A", "199"));
+songList.push(new Song("Pastel * Palletes - Baby Sweet Berry Love", "https://www.youtube.com/embed/6WeBp5G2kUU", "244"));
+songList.push(new Song("Yorushika - Hachigatsu, Bou, Tsukiakari", "https://www.youtube.com/embed/J6eih31MaQ8", "278"));
+songList.push(new Song("Nanahira - Kizuitara Shunkashuutou", "https://www.youtube.com/embed/XRAOSO5m-cY", "295"));
+songList.push(new Song("linear ring - enchanted love", "https://www.youtube.com/embed/qE6mruRB1PA", "130"));
 
 function StartGame() {
     SkipSong();
@@ -42,7 +42,7 @@ function SkipSong() {
             n = Math.trunc(Math.random() * songList.length);
             bool = false;
 
-            for (const numbers of generatedNumbers) { // checks if the generated random number already appeared (avoid repeated songs)
+            for (const numbers of generatedNumbers) { // checks if the generated random number already appeared (avoids repeated songs but is not efficient)
                 if (n == numbers) {
                     bool = true;
                     break;
@@ -52,7 +52,7 @@ function SkipSong() {
             if (bool == false) {
                 generatedNumbers.push(n);
                 sample = Math.trunc(Math.random() * (songList[n].length-30));
-                document.getElementById("showvideo").src = (songList[n].url + "&start=" + sample + "&disablekb=1");
+                document.getElementById("showvideo").src = (songList[n].url + "?rel=0&autoplay=1&start=" + sample + "&disablekb=1");
                 break;
             }
         }
